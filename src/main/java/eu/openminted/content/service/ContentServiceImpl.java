de7f5ab16eb3957837e954097690608c54035710
kgiann78 <kgiann78@gmail.com>
2017-06-15 16:41:10 +0300

    Bug fix: - Fix facet importing and facet merging from CORE and OPENAIRE

--- src/main/java/eu/openminted/content/service/ContentServiceImpl.java
+++ src/main/java/eu/openminted/content/service/ContentServiceImpl.java
@@ -8,9 +8,11 @@ import eu.openminted.registry.domain.Value;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
+import javax.annotation.PostConstruct;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 @Component
 public class ContentServiceImpl implements ContentService {
@@ -18,45 +20,63 @@ public class ContentServiceImpl implements ContentService {
     @Autowired(required = false)
     private List<ContentConnector> contentConnectors;
 
+    private Map<String, String> OmtdFacetLabels = new HashMap<>();
+    String PUBLICATION_TYPE = "publicationType";
+    String PUBLICATION_YEAR = "publicationYear";
+    String PUBLISHER = "publisher";
+    String RIGHTS_STMT_NAME = "rightsStmtName";
+    String LICENCE = "licence";
+    String DOCUMENT_LANG = "documentLanguage";
+    String DOCUMENT_TYPE = "documentType";
+    String SOURCE = "source";
+
+    @PostConstruct
+    private void init() {
+        OmtdFacetLabels.put(PUBLICATION_TYPE,"Publication Type");
+        OmtdFacetLabels.put(PUBLICATION_YEAR,"Publication Year");
+        OmtdFacetLabels.put(RIGHTS_STMT_NAME,"Rights Statement");
+        OmtdFacetLabels.put(LICENCE,"Licence");
+        OmtdFacetLabels.put(DOCUMENT_LANG,"Language");
+        OmtdFacetLabels.put(DOCUMENT_TYPE,"Document Type");
+        OmtdFacetLabels.put(SOURCE,"Content Source");
+    }
+
     @Override
     public SearchResult search(Query query) {
 
         query.setFrom(0);
         query.setTo(1);
 
-        if (query.getFacets() == null) query.setFacets(new ArrayList<>());
-
-        if (!containsIgnoreCase(query.getFacets(), "publicationType")) query.getFacets().add("publicationType");
-        if (!containsIgnoreCase(query.getFacets(), "publicationYear")) query.getFacets().add("publicationYear");
-//        if (!containsIgnoreCase(query.getFacets(), "rightsStmtName")) query.getFacets().add("rightsStmtName");
-        if (!containsIgnoreCase(query.getFacets(), "Licence")) query.getFacets().add("Licence");
-        if (!containsIgnoreCase(query.getFacets(), "documentLanguage")) query.getFacets().add("documentLanguage");
+        if (!query.getFacets().contains(PUBLICATION_TYPE)) query.getFacets().add(PUBLICATION_TYPE);
+        if (!query.getFacets().contains(PUBLICATION_YEAR)) query.getFacets().add(PUBLICATION_YEAR);
+        if (!query.getFacets().contains(LICENCE)) query.getFacets().add(LICENCE);
+        if (!query.getFacets().contains(DOCUMENT_LANG)) query.getFacets().add(DOCUMENT_LANG);
 
         SearchResult result = new SearchResult();
         Facet sourceFacet = new Facet();
         result.setFacets(new ArrayList<>());
 
-        sourceFacet.setField("source");
-        sourceFacet.setLabel("Content Source");
+        sourceFacet.setField(SOURCE);
         sourceFacet.setValues(new ArrayList<>());
 
         // retrieve connectors from query
         List<String> connectors = new ArrayList<>();
-        if (query.getParams().containsKey("source")
-                && query.getParams().get("source") != null
-                && query.getParams().get("source").size() > 0) {
-            connectors.addAll(query.getParams().get("source"));
+        if (query.getParams() != null) {
+            if (query.getParams().containsKey(SOURCE)
+                    && query.getParams().get(SOURCE) != null
+                    && query.getParams().get(SOURCE).size() > 0) {
+                connectors.addAll(query.getParams().get(SOURCE));
+            }
+
+            // remove field query "source" because this is a custom OMTD field
+            if (query.getParams().containsKey(SOURCE))
+                query.getParams().remove(SOURCE);
+            // also remove documentType (for the time being
+            // it is always fullText and the result
+            // will be the same as well)
+            if (query.getParams().containsKey(DOCUMENT_TYPE))
+                query.getParams().remove(DOCUMENT_TYPE);
         }
-
-        // remove field query "source" because this is a custom OMTD field
-        if (query.getParams().containsKey("source"))
-            query.getParams().remove("source");
-        // also remove documentType (for the time being
-        // it is always fullText and the result
-        // will be the same as well)
-        if (query.getParams().containsKey("documentType"))
-            query.getParams().remove("documentType");
-
 
         if (contentConnectors != null) {
             for (ContentConnector connector : contentConnectors) {
@@ -76,6 +96,9 @@ public class ContentServiceImpl implements ContentService {
 
         result.getFacets().add(sourceFacet);
 
+        for (Facet facet : result.getFacets()) {
+            facet.setLabel(OmtdFacetLabels.get(facet.getField()));
+        }
         return result;
     }
 
